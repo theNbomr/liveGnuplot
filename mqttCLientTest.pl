3b6ca926183772de16e4ac92c868412a15a35ec5
Rod Nussbaumer <thenbomr@gmail.com>
2025-05-23 14:32:06 -0700

    First basic working version of MQTT Client

--- mqttCLientTest.pl
+++ mqttCLientTest.pl
@@ -2,5 +2,27 @@
 
 use strict;
 
-use MQTT::Simple;
+use Net::MQTT::Simple;
 
+sub subscriptionHandler($$);
+
+use constant MQTT_BROKER_IP => '192.168.1.100';
+use constant MQTT_BROKER_PORT => '1883';
+use constant MQTT_TOPIC => 'tele/tasmota_F74C1D/SENSOR';
+
+    my $mqttClient = Net::MQTT::Simple->new( MQTT_BROKER_IP
+    
+                                            );
+    $mqttClient->subscribe( MQTT_TOPIC, \&subscriptionHandler );
+    
+    $mqttClient->run();
+
+                                            
+                                            
+sub subscriptionHandler($$){
+
+my ( $topic,$message ) = @_;
+
+    print "TOPIC: $topic, MESSAGE: $message\n";
+
+}
