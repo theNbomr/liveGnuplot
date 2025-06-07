d6d0685885f002f088ae9472b208b64e983cca7d
Rod Nussbaumer <thenbomr@gmail.com>
2025-06-07 13:31:18 -0700

    time/date stamp code & comment cleanup

--- timestampTest.pl
+++ timestampTest.pl
@@ -11,8 +11,8 @@ my @localtimeArgs = [
 #
 #   Use this to lookup the indices into the localtime array 
 #
-    #       0    1    2     3     4    5     6     7     8
-    # my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+#       0    1    2     3     4    5     6     7     8
+# my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my %timeElementLookup = (
     '%S' => 0,
     '%M' => 1,
@@ -53,57 +53,50 @@ my @dowNames = (
     'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
 );
 
-    my( $sec, $min, $hr, $mday, $mon, $year, $dow, $yday, $isdst ) = localtime( time );
-    $year += 1900;
-    $mon += 1;
-    printf( "%04d-%02d-%02dT%02d_%02d_%02d\n", $year, $mon, $mday, $hr, $min, $sec );
-
     my @brokenDownTime = localtime( time );
     $brokenDownTime[ $timeElementLookup{ '%Y'} ] += 1900;
     $brokenDownTime[ $timeElementLookup{ '%m'} ] += 1;
 
-    # foreach my $timeComponent ( @ARGV ){
+    foreach my $timeSpec ( @ARGV ){
 
-    #     if( $timeComponent =~ m/%[YymdHMS]/ ){
-    #         print "Match: $&\n";
-    #         my $timeIndex = $timeElementLookup{ $& };
-    #         my $formatString = $timeFormatLookup{ $& };
-    #         print $formatString;
-    #         printf( " $formatString\n", $brokenDownTime[ $timeIndex ] );
-    #     }
-    # }
+        my @timeElements;  # Build up as the argument list to printf()
 
-    # my $timeSpec = "%Y-%m-%dT%H:%M:%S";
-    my $timeSpec = $ARGV[0];
-    my @timeElements;
-    while( my @matches = $timeSpec =~ m/%[YymdHMSnw]/ ){
-        my $timeSpecMacro = $&;
-        print "timeSpecMacro: $timeSpecMacro\n";
-        my $formatString     = $timeFormatLookup{ $timeSpecMacro };
+        #
+        #   Iteratively scan the given timestamp specification string,
+        #   and lookup the embedded macros (%Y, %m, %S, etc) to find the
+        #   respective localtime elements and also the respective printf()
+        #   format specifiers. Replace the emebedded timestamp macros 
+        #   with the looked-up printf() formatters in the tiemstamp spec string.
+        #
+        while( my @matches = $timeSpec =~ m/%[YymdHMSnw]/ ){
+            my $timeSpecMacro = $&;
+            # print "timeSpecMacro: $timeSpecMacro\n";
 
-        if( $timeSpecMacro eq '%w' ){
-            my $dayName = $dowNames[ $brokenDownTime[ 6 ] ];
-            # print "DayName : $dayName\n";
-            push @timeElements, $dayName;
+            if( $timeSpecMacro eq '%w' ){
+                my $dayName = $dowNames[ $brokenDownTime[ 6 ] ];
+                # print "DayOfWeekName : $dayName\n";
+                push @timeElements, $dayName;
+            }
+            elsif( $timeSpecMacro eq '%n' ){
+                my $monthName = $monthNames[ $brokenDownTime[ 4 ] ] ;
+                # print "MonthIndex: $monthIndex, MonthName: $monthName\n";
+                push @timeElements, $monthName;
+            }
+            elsif( $timeSpecMacro eq '%y' ){
+                my $timeElementIndex = $timeElementLookup{ $timeSpecMacro };
+                push @timeElements, $brokenDownTime[ $timeElementIndex ] % 100;
+            }
+            else{      #  "%Y, %m, %d, %H, %M, %S"
+                my $timeElementIndex = $timeElementLookup{ $timeSpecMacro };
+                push @timeElements, $brokenDownTime[ $timeElementIndex ];
+            }
+
+            # Convert one embedded macro to the respective printf() format string
+            my $formatString = $timeFormatLookup{ $timeSpecMacro };
+            $timeSpec =~ s/$timeSpecMacro/$formatString/;
         }
-        elsif( $timeSpecMacro eq '%n' ){
-            my $monthName = $monthNames[ $brokenDownTime[ 4 ] ] ;
-            # print "MonthIndex: $monthIndex, MonthName: $monthName\n";
-            push @timeElements, $monthName;
-        }
-        elsif( $timeSpecMacro eq '%y' ){
-            my $timeElementIndex = $timeElementLookup{ $timeSpecMacro };
-            push @timeElements, $brokenDownTime[ $timeElementIndex ] % 100;
-        }
-        else{
-            my $timeElementIndex = $timeElementLookup{ $timeSpecMacro };
-            push @timeElements, $brokenDownTime[ $timeElementIndex ];
-        }
-        $timeSpec =~ s/$timeSpecMacro/$formatString/g;
+
+        print "$timeSpec : ";
+        printf( "$timeSpec\n", @timeElements );
     }
-
-    print "$timeSpec : ";
-    printf( "$timeSpec\n", @timeElements );
-
-    # print "Matches: ", scalar @matches, " : $&\n";
     
